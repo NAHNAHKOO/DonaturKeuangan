50ef284083fca7c285bb34b3e78bef3affe956f4
NAHNAHKOO <upinnahh21@gmail.com>
2024-07-04 08:23:34 +0700

    ini

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,64 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\UangMasuk;
 
-class dashboardController extends Controller
+class Dashboard1Controller extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $data = UangMasuk::all();
-        return view('dashboard.index',compact('data'));
-    }
+        $UangMasuk = Category::count();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        // Tambahkan query sum disini, pakai query builder punya laravel
+        $uang_masuk = DB::table('keuangan')
+                ->select('department', DB::raw('SUM(price) as total_sales'))
+                ->get();
+        $posts = Post::count();
+        
+        return view('dashboard.index', compact('UangMasuk'));
     }
 }
+
